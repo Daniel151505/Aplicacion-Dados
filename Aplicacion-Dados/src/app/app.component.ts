4cd24656aaa573f331726749cb12659dd98994ff
Daniel151505 <daniel150520011@gmail.com>
2021-02-12 12:31:06 -0500

    Metodo Tirar Dados Completo

--- Aplicacion-Dados/src/app/app.component.ts
+++ Aplicacion-Dados/src/app/app.component.ts
@@ -15,6 +15,8 @@ export class AppComponent {
   tirarDados(): void {
     this.numero1= Math.round(Math.random() * 5) + 1 ;
     this.numero2= Math.round(Math.random() * 5) + 1 ;
+    this.dadoIzquierda= '../assets/images/dice' + this.numero1 + ".png";
+    this.dadoDerecha= '../assets/images/dice' + this.numero2 + ".png"
   }
 
 }
